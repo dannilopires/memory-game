504eaf22996b0d59d49998f81daa1c97c625fbe1
dannilo <dannilo-pires@hotmail.com>
2022-07-24 10:01:36 -0300

    cria functions js para os cards

--- script.js
+++ script.js
@@ -1,9 +1,40 @@
 const cards = document.querySelectorAll('.card');
 
+let hasFlippedCard = false;
+let firstCard, secondCard;
+
 function flipCard(){
     this.classList.add('flip');
+    if (!hasFlippedCard) {
+        hasFlippedCard = true;
+        firstCard = this;
+        return;
+    }
+
+    secondCard = this;
+    hasFlippedCard = false;
+    checkForMath();
+}
+
+function checkForMath(){
+    if(firstCard.dataset.card === secondCard.dataset.card) {
+        disableCards();
+        return;
+    }
+
+    unflipCards();
+}
+
+function disableCards(){
+
+}
+
+function unflipCards(){
+
 }
 
 cards.forEach((card) => {
     card.addEventListener('click', flipCard)
 })
+
+
